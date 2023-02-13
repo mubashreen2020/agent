9d67a4ae7314fc12594162b3790ca7a1ff9ab455
Mujffar <50290716+Mujffar@users.noreply.github.com>
2023-02-13 14:18:21 +0530

    Update ginterface.py

--- interfaces/ginterface.py
+++ interfaces/ginterface.py
@@ -1,5 +1,8 @@
 import psutil
 import socket
+import requests
+
+
 
 def get_network_info():
     # Get information about the network interfaces
@@ -12,10 +15,17 @@ def get_network_info():
 
         # If the interface has an IPv4 address
         if ipv4_address:
-            # Get the public IP address
-            public_ip = socket.gethostbyname(socket.gethostname())
 
-            interfacearray = (interface_name,{'Loopback' if interface_name == 'lo' else 'Physical'},{ipv4_address},{public_ip})
+def get_public_ip():
+    # Make a request to a public IP address API
+    response = requests.get("https://api.ipify.org")
+    public_ip = response.text
+    
+    return public_ip
+
+public_ip = get_public_ip()
+
+            interfacearray = (name:interface_name,'Loopback' if interface_name == 'lo' else 'Physical',ipv4_address,public_ip)
             print(interfacearray)
             # Print the information about the interface
             print(f"Name: {interface_name}\nType: {'Loopback' if interface_name == 'lo' else 'Physical'}\nIPv4 Address: {ipv4_address}\nPublic IP: {public_ip}\n")

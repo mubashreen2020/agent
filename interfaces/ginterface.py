7b5c4f2bad70ef15bd09a64cb2d284cd999166e0
Mujffar <50290716+Mujffar@users.noreply.github.com>
2023-02-13 14:39:12 +0530

    Update ginterface.py

--- interfaces/ginterface.py
+++ interfaces/ginterface.py
@@ -1,8 +1,5 @@
 import psutil
 import socket
-import requests
-
-
 
 def get_network_info():
     # Get information about the network interfaces
@@ -13,16 +10,13 @@ def get_network_info():
         # Get the first IPv4 address of the interface
         ipv4_address = next((address.address for address in interface_addresses if address.family == socket.AF_INET), None)
 
-def get_public_ip():
-    # Make a request to a public IP address API
-    response = requests.get("http://checkip.dyndns.org").text
-    # Extract the public IP address from the response
-    public_ip = response.split("<body>")[1].split("</body>")[0].strip()
-    
-    return public_ip
+        # If the interface has an IPv4 address
+        if ipv4_address:
+            # Get the public IP address
+            public_ip = socket.gethostbyname(socket.gethostname())
 
-public_ip = get_public_ip()
-
+            # Print the information about the interface
+            print(f"Name: {interface_name}\nType: {'Loopback' if interface_name == 'lo' else 'Physical'}\nIPv4 Address: {ipv4_address}\nPublic IP: {public_ip}\n")
 
 # Print the information about the network interfaces
 get_network_info()

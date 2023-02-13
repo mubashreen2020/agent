3698a1c43bdb9a22d70b76e9cdd6d215c772de17
Mujffar <50290716+Mujffar@users.noreply.github.com>
2023-02-13 14:46:23 +0530

    Update ginterface.py

--- interfaces/ginterface.py
+++ interfaces/ginterface.py
@@ -6,7 +6,7 @@ def get_public_ip():
     # Make a request to a public IP address API
     response = requests.get("http://checkip.dyndns.org").text
     # Extract the public IP address from the response
-    public_ip = response.split("<body>")[1].split("</body>")[0].strip()
+    public_ip = response.split("<body>").split("</body>")[0].strip()
     
     return public_ip
 

17eec9fb39c1604c32648b4b5454548d932e6a22
Mujffar <50290716+Mujffar@users.noreply.github.com>
2023-02-13 10:36:32 +0530

    Update vpp_stop.py

--- vpp/vpp_stop.py
+++ vpp/vpp_stop.py
@@ -7,3 +7,21 @@ def stop_vpp():
 
 # Stop the VPP service
 stop_vpp()
+
+def is_vpp_running():
+    try:
+        # Use subprocess to run the "pgrep" command
+        output = subprocess.check_output("pgrep vpp", shell=True)
+        # If VPP is running, the output of the command will not be empty
+        if output:
+            return True
+    except subprocess.CalledProcessError:
+        # If VPP is not running, the "pgrep" command will return a non-zero exit code
+        pass
+
+    return False
+
+if is_vpp_running():
+    print("VPP is running")
+else:
+    print("VPP is not running")

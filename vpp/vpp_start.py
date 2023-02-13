be435b11e79cb2b86ce7baf03d0787dad69ba69c
Mujffar <50290716+Mujffar@users.noreply.github.com>
2023-02-13 10:32:30 +0530

    Update vpp_start.py

--- vpp/vpp_start.py
+++ vpp/vpp_start.py
@@ -3,9 +3,4 @@ import subprocess
 # Start the VPP service
 subprocess.run(["service", "vpp", "start"])
 
-# Check the status of the VPP service
-result = subprocess.run(["service", "vpp", "status"], capture_output=True)
-
-# Print the status of the VPP service
-print(result.stdout.decode().strip())
 

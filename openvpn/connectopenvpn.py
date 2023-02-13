352076deebb6cde1baeb6ed9bf6b16379bb11578
Mujffar <50290716+Mujffar@users.noreply.github.com>
2023-02-13 10:51:03 +0530

    Update connectopenvpn.py

--- openvpn/connectopenvpn.py
+++ openvpn/connectopenvpn.py
@@ -3,4 +3,7 @@ import subprocess
 
 # Start the OpenVPN service
 subprocess.run(["service", "openvpn", "start", "server"])
+        print("OpenVPN has connected Successfully.")
+    except subprocess.CalledProcessError as e:
+        print("Error: Failed to disconnect OpenVPN. Error code: {}".format(e.returncode))
 

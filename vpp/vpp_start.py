57dd066597170f9045d3f186d73b65236366cd86
Mujffar <50290716+Mujffar@users.noreply.github.com>
2023-02-13 10:36:00 +0530

    Update vpp_start.py

--- vpp/vpp_start.py
+++ vpp/vpp_start.py
@@ -4,3 +4,21 @@ import subprocess
 subprocess.run(["service", "vpp", "start"])
 
 
+
+def is_vpp_running():
+    try:
+        # Use subprocess to run the "pgrep" command
+        output = subprocess.check_output("pgrep vpp", shell=True)
+        # If VPP is running, the output of the command will not be empty
+        if output:
+            return True
+    except subprocess.CalledProcessError:
+        # If VPP is not running, the "pgrep" command will return a non-zero exit code
+        pass
+
+    return False
+
+if is_vpp_running():
+    print("VPP is running")
+else:
+    print("VPP is not running")

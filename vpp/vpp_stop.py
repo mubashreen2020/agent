ae7734d1eec83059cfea5608bf3d2fc4cf248fdc
Mujffar <50290716+Mujffar@users.noreply.github.com>
2023-02-13 10:32:53 +0530

    Update vpp_stop.py

--- vpp/vpp_stop.py
+++ vpp/vpp_stop.py
@@ -4,11 +4,6 @@ def stop_vpp():
     # Stop the VPP service
     subprocess.run(["service", "vpp", "stop"])
 
-    # Check the status of the VPP service
-    result = subprocess.run(["service", "vpp", "status"], capture_output=True)
-
-    # Print the status of the VPP service
-    print(result.stdout.decode().strip())
 
 # Stop the VPP service
 stop_vpp()

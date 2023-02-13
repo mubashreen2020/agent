5147f06eb2e9a76d11f08f13b1f90a7e8455c42d
Mujffar <50290716+Mujffar@users.noreply.github.com>
2023-02-13 14:32:31 +0530

    Update publicip.py

--- interfaces/publicip.py
+++ interfaces/publicip.py
@@ -3,13 +3,10 @@ import requests
 def get_public_ip():
     # Make a request to a public IP address API
     response = requests.get("http://checkip.dyndns.org").text
-    
     # Extract the public IP address from the response
-    public_ip = response.strip().split(" ")[-1]
+    public_ip = response.split("<body>")[1].split("</body>")[0].strip()
     
     return public_ip
 
-# Get the public IP address
 public_ip = get_public_ip()
-
 print(public_ip)

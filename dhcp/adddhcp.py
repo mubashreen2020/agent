797c239a6d9f0778e9f75c9e2965b9a099ab709a
Mujffar <50290716+Mujffar@users.noreply.github.com>
2023-02-14 12:24:44 +0530

    Update adddhcp.py

--- dhcp/adddhcp.py
+++ dhcp/adddhcp.py
@@ -1,42 +1,24 @@
 import subprocess
 
-# Define the DHCP configuration file path
-dhcp_conf_file = '/etc/dhcp/dhcpd.conf'
-
-# Define the DHCP service name
-dhcp_service_name = 'isc-dhcp-server'
-
-# Define the DHCP interface name
-dhcp_interface = 'enp0s8'
-
-# Define the DHCP IP range start
-dhcp_ip_range_start = '192.168.3.100'
-
-# Define the DHCP IP range end
-dhcp_ip_range_end = '192.168.3.200'
-
-# Define the DHCP DNS server
-dhcp_dns_server = '8.8.8.8'
-
-# Define the DHCP configuration
-dhcp_config = '''
-subnet 192.168.3.0 netmask 255.255.255.0 {
-  range {}; {}
-  option broadcast-address 192.168.3.255;
-  option routers 192.168.3.1;
-  option domain-name-servers {};
-}
-'''.format(dhcp_ip_range_start, dhcp_ip_range_end, dhcp_dns_server)
-
-# Write the DHCP configuration to the file
-with open(dhcp_conf_file, 'w') as f:
-    f.write(dhcp_config)
-
-# Restart the DHCP service
-subprocess.call(['sudo', 'systemctl', 'restart', dhcp_service_name])
-
-# Enable the DHCP service
-subprocess.call(['sudo', 'systemctl', 'enable', dhcp_service_name])
-
-# Add the DHCP interface
-subprocess.call(['sudo', 'ifconfig', dhcp_interface, 'up'])
+# Configure DHCP
+def configure_dhcp(interface, start_ip, end_ip, dns_server):
+    # Create the dhcp configuration file
+    with open("/etc/dhcpd.conf", "w") as file:
+        file.write("subnet 192.168.3.0 netmask 255.255.255.0 {\n")
+        file.write("  range 192.168.3.100 192.168.3.150;\n")
+        file.write("  option broadcast-address 192.168.3.255;\n")
+        file.write("  option routers 192.168.3.1;\n")
+        file.write("  default-lease-time 600;\n")
+        file.write("  max-lease-time 7200;\n")
+        file.write("  option domain-name-servers google.com, google.com;\n")
+        file.write("}\n")
+    
+    # Enable the DHCP service
+    subprocess.run(["systemctl", "enable", "dhcpd"])
+    subprocess.run(["systemctl", "start", "dhcpd"])
+    
+    # Restart the network service
+    subprocess.run(["systemctl", "restart", "network"])
+    
+# Call the function
+configure_dhcp("eth0", "192.168.3.100", "192.168.3.150", "google.com,google.com")

825aa718a53f8ebc64ccc7b255b52aa9383e7e91
Mujffar <50290716+Mujffar@users.noreply.github.com>
2023-02-15 14:34:57 +0530

    Create test.py

---
+++ interfaces/test.py
@@ -0,0 +1,17 @@
+import pymongo
+
+# set up the MongoDB client and database
+client = pymongo.MongoClient("mongodb+srv://mj:FsR47oq0d2OjwXkt@cluster0.cc99ozj.mongodb.net/test")
+db = client.test
+
+# select the collection to insert into
+collection = db.my_collection
+
+# create the record to insert
+record = {"name": "Mujffar Sayyad", "email": "mj@example.com"}
+
+# insert the record into the collection
+result = collection.insert_one(record)
+
+# print the unique identifier (_id) of the inserted record
+print(result.inserted_id)

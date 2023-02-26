bf9b97c7dd653aa8cdcbfd2fe6eeed5b35607771
Mujffar <50290716+Mujffar@users.noreply.github.com>
2023-02-26 19:44:22 +0530

    Update app.py

--- reachedge/app.py
+++ reachedge/app.py
@@ -1,15 +1,7 @@
-from flask import Flask
+from flask import Flask, render_template
+
 app = Flask(__name__)
-app = Flask(__name__, static_url_path='/static')
 
 @app.route('/')
-def home():
-    return 'Welcome to the home page!'
-
-@app.route('/users')
-def users():
-    return 'List of users'
-
-
-if __name__ == '__main__':
-    app.run(host='192.168.1.8')
+def index():
+    return render_template('index.html')

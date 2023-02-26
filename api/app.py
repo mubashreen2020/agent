b36552ee77eb158e4fb973051ea9ce4271b1703c
Mujffar <50290716+Mujffar@users.noreply.github.com>
2023-02-26 20:11:20 +0530

    Update app.py

--- api/app.py
+++ api/app.py
@@ -1,11 +1,10 @@
-from flask import Flask, request
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
-@app.route('/hello')
-def hello():
-    name = request.args.get('name', 'World')
-    return f'Hello, {name}!'
+@app.route('/hello', methods=['GET'])
+def hello_world():
+    return jsonify({'message': 'Hello, World!'})
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='192.168.1.8', port=5000)

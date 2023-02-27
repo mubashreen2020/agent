6540afc0fd48b82e794b68ece8a042cf2c4ff70c
Mujffar <50290716+Mujffar@users.noreply.github.com>
2023-02-27 18:13:43 +0530

    Delete networkinterface.py

--- systemchecker/networkinterface.py
+++
@@ -1,9 +0,0 @@
-import netifaces
-
-interfaces = netifaces.interfaces()
-
-if len(interfaces) < 2:
-    print("At least 2 network interfaces are required.")
-else:
-    print("There are at least 2 network interfaces.")
-

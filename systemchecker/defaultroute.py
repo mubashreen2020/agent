3b5b59066e0dffadd1fc898a2939081b4766162c
Mujffar <50290716+Mujffar@users.noreply.github.com>
2023-02-27 14:13:30 +0530

    Add files via upload

--- systemchecker/defaultroute.py
+++ systemchecker/defaultroute.py
@@ -1,23 +1,14 @@
 import subprocess
 
 def check_default_route():
-    # Run the "ip route" command to get the default route
-    output = subprocess.check_output(["ip", "route"])
-    lines = output.decode().split('\n')
-    default_route = None
-    for line in lines:
-        if line.startswith("default via "):
-            default_route = line.split()[2]
-            break
-
-    # Ping a well-known IP address on the Internet
-    ping_result = subprocess.call(["ping", "-c", "1", "-W", "1", default_route])
-
-    # Check if the ping was successful
-    if ping_result == 0:
-        print("Default route has WAN connectivity")
-    else:
-        print("Default route does not have WAN connectivity")
+    output = subprocess.check_output(['ip', 'route', 'show']).decode()
+    for line in output.split('\n'):
+        if 'default via' in line:
+            return True
+    return False
 
 if __name__ == '__main__':
-    check_default_route()
+    if check_default_route():
+        print('Default route exists.')
+    else:
+        print('No default route found.')

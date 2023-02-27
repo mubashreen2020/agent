30df2cffb8ce50878b0309dde88c6631e1d5f6b4
Mujffar <50290716+Mujffar@users.noreply.github.com>
2023-02-27 16:09:08 +0530

    Add files via upload

--- systemchecker/defroutemetric.py
+++ systemchecker/defroutemetric.py
@@ -1,14 +1,32 @@
 import subprocess
 
-# Execute the ip route command
-output = subprocess.check_output(["ip", "route"])
+def get_default_route_metric():
+    # Execute the "ip route show" command and capture its output
+    ip_route_output = subprocess.check_output(["ip", "route", "show"]).decode()
 
-# Parse the output to find the default route metric
-for line in output.decode().splitlines():
-    if "default via" in line:
-        fields = line.split()
-        metric_index = fields.index("metric") + 1
-        default_metric = int(fields[metric_index])
-        break
+    # Split the output into lines
+    ip_route_lines = ip_route_output.split("\n")
 
-print("Default route metric:", default_metric)
+    # Find the line that contains the default route
+    default_route_line = None
+    for line in ip_route_lines:
+        if "default via" in line:
+            default_route_line = line
+            break
+
+    # Parse the default route line to extract the metric value
+    if default_route_line is not None:
+        metric_index = default_route_line.find("metric")
+        if metric_index != -1:
+            metric_str = default_route_line[metric_index:].split()[1]
+            return int(metric_str)
+
+    # If no default route was found or it didn't have a metric, return None
+    return None
+
+# Example usage:
+metric = get_default_route_metric()
+if metric is not None:
+    print(f"The default route metric is {metric}.")
+else:
+    print("No default route found.")

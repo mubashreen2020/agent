74e92e0e93a7b4315e3841bba7a906b0ab72d89d
Mujffar <50290716+Mujffar@users.noreply.github.com>
2023-03-01 12:05:32 +0530

    Add files via upload

--- systemchecker/hostnamesyntax.py
+++ systemchecker/hostnamesyntax.py
@@ -1,19 +1,20 @@
 import re
 
-def is_valid_hostname(hostname):
+def check_hostname_syntax(hostname):
     """
-    Returns True if the given string is a valid hostname, False otherwise.
+    Check the syntax of a hostname in Linux.
+    Returns True if the syntax is valid, False otherwise.
     """
     if len(hostname) > 255:
         return False
-    if hostname[-1] == ".":
-        hostname = hostname[:-1]  # strip exactly one dot from the right, if present
-    allowed = re.compile("(?!-)[A-Z\d-]{1,63}(?<!-)$", re.IGNORECASE)
-    return all(allowed.match(x) for x in hostname.split("."))
 
-# Example usage
-hostname = "www.example.com"
-if is_valid_hostname(hostname):
-    print(f"{hostname} is a valid hostname")
-else:
-    print(f"{hostname} is not a valid hostname")
+    if hostname[-1] == ".":
+        hostname = hostname[:-1]
+
+    # Valid hostname regex
+    hostname_regex = re.compile(r'^[a-zA-Z0-9]([a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(\.[a-zA-Z0-9]([a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$')
+
+    if not hostname_regex.match(hostname):
+        return False
+
+    return True

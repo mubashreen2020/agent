b30703f4b4c53e9688067f1dbd5f7df6280a34b5
Mujffar <50290716+Mujffar@users.noreply.github.com>
2023-03-01 15:42:38 +0530

    Add files via upload

--- systemchecker/duplicatenetplann.py
+++ systemchecker/duplicatenetplann.py
@@ -1,21 +1,14 @@
-import os
+import yaml
 
-def check_duplicate_sections():
-    netplan_dir = "/etc/netplan"
-    config_files = [f for f in os.listdir(netplan_dir) if os.path.isfile(os.path.join(netplan_dir, f)) and f.endswith(".yaml")]
-    
-    sections = {}
-    for file in config_files:
-        with open(os.path.join(netplan_dir, file), "r") as f:
-            content = f.read()
-            
-            for line in content.split("\n"):
-                if line.strip().startswith("network:"):
-                    section_name = line.strip().split(":")[1].strip()
-                    if section_name in sections:
-                        return f"Duplicate section '{section_name}' found in {file} and {sections[section_name]}."
-                    sections[section_name] = file
-    
-    return "No duplicate sections found in netplan configuration."
+def check_duplicate_netplan_sections():
+    with open('/etc/netplan/*.yaml', 'r') as f:
+        data = yaml.safe_load_all(f)
+        sections = {}
+        for doc in data:
+            if 'network' in doc:
+                if doc['network'] in sections:
+                    print(f"Error: Duplicate netplan section found: {doc['network']}")
+                else:
+                    sections[doc['network']] = True
 
-print(check_duplicate_sections())
+check_duplicate_netplan_sections()

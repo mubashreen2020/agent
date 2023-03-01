01393d81951547030014dd1b57a71674e7fa1b15
Mujffar <50290716+Mujffar@users.noreply.github.com>
2023-03-01 10:32:00 +0530

    Delete networkcards.py

--- systemchecker/networkcards.py
+++
@@ -1,8 +0,0 @@
-import netifaces
-
-# Retrieve a list of network interfaces and their associated MAC addresses
-interfaces = netifaces.interfaces()
-for interface in interfaces:
-    if interface != 'lo':
-        mac_address = netifaces.ifaddresses(interface)[netifaces.AF_LINK][0]['addr']
-        print(f"Interface {interface} has MAC address {mac_address}")

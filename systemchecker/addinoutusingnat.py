f0119379144882174e23f78603646c19bf0b2b19
mubashreen2020 <126747281+mubashreen2020@users.noreply.github.com>
2023-03-04 11:27:01 +0530

    Delete addinoutusingnat.py

--- systemchecker/addinoutusingnat.py
+++
@@ -1,9 +0,0 @@
-import subprocess
-
-external_interface = 'eth0'
-internal_ip = '192.168.1.100'
-external_port = '80'
-internal_port = '8080'
-
-subprocess.run(['iptables', '-A', 'PREROUTING', '-t', 'nat', '-i', external_interface, '-p', 'tcp', '--dport', external_port, '-j', 'DNAT', '--to-destination', f'{internal_ip}:{internal_port}'])
-subprocess.run(['iptables', '-A', 'FORWARD', '-p', 'tcp', '-d', internal_ip, '--dport', internal_port, '-j', 'ACCEPT'])
